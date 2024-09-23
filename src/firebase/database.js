697bd1156e0261beee9cfc034b9ab365aa75d08b
Raul Ruiz <rruizf@bch.bancodechile.cl>
2024-09-23 00:14:05 -0300

    feat:update de autorizacion de login

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,3 +1,3 @@
 export const URL_FIREBASE = "https://ecommerce-2ee8f-default-rtdb.firebaseio.com/"
-export const API_AUTH = "AIzaSyCyAHZGZvXEO-A1WIonEdlJhRZeD1XpLDw"
+export const API_AUTH = "AIzaSyBMxHgesFVQpuWM7Z4IGAmkQo-M-Er85fs"
 export const URL_AUTH = "https://identitytoolkit.googleapis.com/v1/"
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,37 +1,49 @@
-import { StyleSheet } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import AuthStack from './AuthStack'
-import TabNavigator from './TabNavigator'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchSession } from '../db'
-import { setUser } from '../features/auth/authSlice'
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack';
+import TabNavigator from './TabNavigator';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSession } from '../db';
+import { setUser } from '../features/auth/authSlice';
 
 const MainNavigator = () => {
+  const idToken = useSelector((state) => state.auth.idToken); // Estado de autenticación
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true); // Estado de carga
 
-    const idToken = useSelector(state => state.auth.idToken)
-    const dispatch = useDispatch()
-    
-    useEffect(()=>{
-      (async ()=>{
-        const sessions = await fetchSession()
-        if(sessions){
-          dispatch(setUser(sessions))
-        }
-      
-      })()
-    },[])
+  useEffect(() => {
+    (async () => {
+      const sessions = await fetchSession(); // Obtener sesión desde la base de datos
+      if (sessions) {
+        dispatch(setUser(sessions)); // Si hay sesión, cargarla en el store
+      }
+      setLoading(false); // Finalizar el estado de carga
+    })();
+  }, []);
+
+  if (loading) {
+    // Mostrar spinner mientras se verifica la sesión
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
-   <NavigationContainer>
-    {idToken ? <TabNavigator/>  : <AuthStack/> }
-    
+    <NavigationContainer>
+      {idToken ? <TabNavigator /> : <AuthStack />} 
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default MainNavigator
+export default MainNavigator;
 
 const styles = StyleSheet.create({
-  
-  })
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
